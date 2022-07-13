1f154d3e2a64df45fb84e45c7d9dc558fbbb0711
AnggiSatria <anggisatria122@gmail.com>
2022-07-13 21:49:07 +0700

    updated

--- Jawaban No  4/4.js
+++ Jawaban No  4/4.js
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 

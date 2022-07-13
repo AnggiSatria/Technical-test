78082263d665d238d6a3c15ba28f39acfb83fc84
AnggiSatria <anggisatria122@gmail.com>
2022-07-13 20:36:40 +0700

    updated

--- Jawaban No 2/2.js
+++ Jawaban No 2/2.js
@@ -1,5 +1,4 @@
-const jumlahBelanja = 100000
-
+const jumlahBelanja = 150000
 
 function DumbWaysJos() {
     const potongan = 21.1 / 100
@@ -7,14 +6,17 @@ function DumbWaysJos() {
     const maksDiskon = 20000
     const diskon = jumlahBelanja * potongan
 
-
     if(jumlahBelanja > minBelanja){
-        if( jumlahBelanja > maksDiskon ){
+        if( jumlahBelanja < 100000 ){
                 const totalDiskonDibawah20000 = jumlahBelanja - diskon
-                console.log(totalDiskonDibawah20000);
+                console.log(`total belanja adalah ${totalDiskonDibawah20000}`);
+                const kembalian1 = jumlahBelanja - totalDiskonDibawah20000
+                console.log(`total kembalian adalah ${kembalian1}`);
             }else{
                 const totalDiskonDiatas20000 = jumlahBelanja - maksDiskon
-                console.log(totalDiskonDiatas20000);
+                console.log(`total belanja adalah  ${totalDiskonDiatas20000}`);
+                const kembalian2 = jumlahBelanja - totalDiskonDiatas20000
+                console.log(`total belanja adalah  ${kembalian2}`);
             }
         }else{
                 console.log('Anda Belum Berhak Mendapatkan Diskon');
@@ -31,12 +33,16 @@ function DumbWaysJos() {
         const diskon = jumlahBelanja * potongan
        
             if( jumlahBelanja > minBelanja ){
-                if( jumlahBelanja < maksDiskon ){
+                if( jumlahBelanja < 133000 ){
                     const diatasDiskon = jumlahBelanja - diskon
-                    console.log(diatasDiskon);
+                    console.log(`total belanja adalah ${diatasDiskon}`);
+                    const kembalian = jumlahBelanja - diatasDiskon
+                    console.log(`total kembalian adalah ${kembalian}`);
                 }else{
                     const dibawahDiskon = jumlahBelanja - maksDiskon
-                    console.log(dibawahDiskon);
+                    console.log(`total belanja adalah ${dibawahDiskon}`);
+                    const kembalian2 = jumlahBelanja - dibawahDiskon
+                    console.log(kembalian2);
                 }
             }else{
                 console.log('anda tidak dapat diskon');
@@ -46,5 +52,3 @@ function DumbWaysJos() {
     }
 
    DumbWaysMantap() 
-
-
